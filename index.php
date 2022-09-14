6f6c3be9542f4f0c6b88435ccdcef471124f6eee
Bertrand Gorge <bertrand.gorge@neayi.com>
2022-09-14 12:19:00 +0200

    Added GA

--- index.php
+++ index.php
@@ -136,6 +136,18 @@ function sendEmail($rows)
   <link href="css/styles.css" rel="stylesheet">
 
   <title>Aseica account retrieval</title>
+
+
+  <!-- Global site tag (gtag.js) - Google Analytics -->
+  <script async src="https://www.googletagmanager.com/gtag/js?id=UA-241152566-1"></script>
+  <script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+
+    gtag('config', 'UA-241152566-1');
+  </script>
+
 </head>
 
 <body>
